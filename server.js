d8bea66cce310a8958da32d9d87781bca0810f76
AbigailXu <qbxzz233@gmail.com>
2024-07-27 01:17:16 -0400

    allow all origin

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware /errorHandler");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 
 connectDb();
 const app = express();
 
 const port = process.env.PORT || 5001;
+
+// CORS configuration for all routes and origins
+// TODO: REMOVE ALLOW ALL ORIGIN BEFORE RELEASE
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies to be sent with requests
+}));
+
 
 app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
