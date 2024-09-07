4bc3028cd861bfbf8528388cbb577a3cad615c1b
Sheng Liang <shengliangchow@gmail.com>
2024-09-06 21:07:23 -0400

    Added hello route

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
+app.use("/api/hello", require("./routes/helloRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
