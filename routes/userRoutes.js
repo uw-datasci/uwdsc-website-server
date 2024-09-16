7cd4fcbb864d5a6aee72cb43d62b4ce110db0fc7
Sheng Liang <shengliangchow@gmail.com>
2024-09-15 21:23:48 -0400

    verfication and forgot pass implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,10 @@ const {
   registerUser,
   currentUser,
   loginUser,
+  verifyUser,
+  forgotPassword,
+  resetPassword,
+  getQr,
 } = require("../controllers/userController");
 const { validateToken } = require("../middleware /validateTokenHandler");
 
@@ -10,7 +14,15 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 
+router.post("/forgotPass", forgotPassword);
+
+router.post("/resetPass", resetPassword);
+
 router.post("/login", loginUser);
+
+router.post("/verifyUser", verifyUser);
+
+router.get("/getQr", validateToken, getQr);
 
 router.get("/current", validateToken, currentUser);
 
