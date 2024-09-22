3d30c607cfa661e2491b83a6d89c84666a226891
Sheng Liang <shengliangchow@gmail.com>
2024-09-22 15:01:44 -0400

    deploy fix 2

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
-const dotenv = require("dotenv").config();
 const cors = require('cors');
-const sendEmail = require('./util/sendEmail');
-const asyncHandler = require('express-async-handler');
 
 connectDb();
 const app = express();
 
-const port = 5001; // Ensure this matches the port your server is running on
+const port = process.env.PORT || 5001; // Ensure this matches the port your server is running on
 
 // CORS configuration for all routes and origins
 // TODO: REMOVE ALLOW ALL ORIGIN BEFORE RELEASE
@@ -24,19 +21,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/hello", require("./routes/helloRoutes"));
 app.use("/api/resend-verification", require("./routes/verificationRoutes"));
-
-// Email sending route (wrapped in asyncHandler for error handling)
-app.get('/send-email', asyncHandler(async (req, res) => {
-  const result = await sendEmail(); // Assuming this function is defined to send an email
-  res.status(200).json(result);
-}));
-
 app.use(errorHandler);
-
-app.get('/send-email', async (req, res) => {
-    const result = await sendEmail();
-    res.json(result);
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
