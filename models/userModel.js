4b49988078320f1c3f57cddc58b6eb4fcc1fa6b2
JUSTIN_WANG <justin.wang1ab@gmail.com>
2025-01-09 00:45:54 -0500

    add function to push new user account onto registrants list of events that don't require registration

--- models/userModel.js
+++ models/userModel.js
@@ -1,109 +1,136 @@
 const mongoose = require("mongoose");
+const Events = mongoose.model('events');
 
 const userSchema = mongoose.Schema(
-{
-    username: {
-      type: String,
-      required: [true, "Please add the user name"],
-    },
-    email: {
-        type: String,
-        required: [true, "Please add the user Waterloo email address"],
-        unique: [true, "Waterloo Email already exists"]
-    },
-    password: {
-        type: String,
-        required: [true, "Please add the user password"],
-    },
-    userStatus: {
-        type: String,
-        enum: ['member', 'admin'],
-        required: [true, "Please add the user's status"],
-        default: 'member'
-    },
-    hasPaid: {
-        type: Boolean,
-        required: [true, "Please add whether the user has paid or not"],
-        default: false
-    },
-    watIAM: {
-        type: String,
-        required: [true, "Please add the user watIAM"],
-        unique: [true, "watIAM already exists"]
-    },
-    faculty: {
-        type: String,
-        required: [true, "Please add the user faculty"],
-        enum: ["Math", "Engineering", "Science", "Arts", "Health", "Environment"]
-    },
-    term: {
-        type: String,
-        required: [true, "Please add the user term"]
-    },
-    heardFromWhere: {
-        type: String,
-        required: [true, "Please add where the user heard about us from"],
-        default: "",
-    },
-    paymentMethod: {
-        type: String,
-        enum: ["Cash", "Online", "MathSoc"],
-        validate: {
-            validator: (value) => {
-                if (this.hasPaid && !value) {
-                    return false;
-                }
-                return true;
-            },
-            message: "You need a payment method if a user has paid",
-        },
-    },
-    paymentLocation: {
-        type: String,
-        validate: {
-            validator: (value) => {
-                if (this.hasPaid && !value) {
-                    return false;
-                }
-                return true;
-            },
-            message: "You need a payment location if a user has paid",
-        },
-    },
-    verifier: {
-        type: String,
-        validate: {
-            validator: (value) => {
-                if (this.hasPaid && !value) {
-                    return false;
-                }
-                return true;
-            },
-            message: "You need a payment verifier if a user has paid",
-        },
-    },
-    isEmailVerified: {
-        type: Boolean,
-        required: [true, "Please add if the user's email is verified"],
-        default: false,
-    },
-    memberIdeas: {
-        type: String
-    },
-    token: {
-        hash: {
+    {
+        username: {
             type: String,
-            default: "somehash"
+            required: [true, "Please add the user name"],
         },
-        expires: {
-            type: Number,
-            default: -1
+        email: {
+            type: String,
+            required: [true, "Please add the user Waterloo email address"],
+            unique: [true, "Waterloo Email already exists"]
         },
+        password: {
+            type: String,
+            required: [true, "Please add the user password"],
+        },
+        userStatus: {
+            type: String,
+            enum: ['member', 'admin'],
+            required: [true, "Please add the user's status"],
+            default: 'member'
+        },
+        hasPaid: {
+            type: Boolean,
+            required: [true, "Please add whether the user has paid or not"],
+            default: false
+        },
+        watIAM: {
+            type: String,
+            required: [true, "Please add the user watIAM"],
+            unique: [true, "watIAM already exists"]
+        },
+        faculty: {
+            type: String,
+            required: [true, "Please add the user faculty"],
+            enum: ["Math", "Engineering", "Science", "Arts", "Health", "Environment"]
+        },
+        term: {
+            type: String,
+            required: [true, "Please add the user term"]
+        },
+        heardFromWhere: {
+            type: String,
+            required: [true, "Please add where the user heard about us from"],
+            default: "",
+        },
+        paymentMethod: {
+            type: String,
+            enum: ["Cash", "Online", "MathSoc"],
+            validate: {
+                validator: (value) => {
+                    if (this.hasPaid && !value) {
+                        return false;
+                    }
+                    return true;
+                },
+                message: "You need a payment method if a user has paid",
+            },
+        },
+        paymentLocation: {
+            type: String,
+            validate: {
+                validator: (value) => {
+                    if (this.hasPaid && !value) {
+                        return false;
+                    }
+                    return true;
+                },
+                message: "You need a payment location if a user has paid",
+            },
+        },
+        verifier: {
+            type: String,
+            validate: {
+                validator: (value) => {
+                    if (this.hasPaid && !value) {
+                        return false;
+                    }
+                    return true;
+                },
+                message: "You need a payment verifier if a user has paid",
+            },
+        },
+        isEmailVerified: {
+            type: Boolean,
+            required: [true, "Please add if the user's email is verified"],
+            default: false,
+        },
+        memberIdeas: {
+            type: String
+        },
+        token: {
+            hash: {
+                type: String,
+                default: "somehash"
+            },
+            expires: {
+                type: Number,
+                default: -1
+            },
+        }
+    },
+    {
+        timestamps: true,
     }
-},
-{
-    timestamps: true,
-}
 );
+
+userSchema.pre("save", async function (next) {
+    const openEventIds = await Events.find({
+        $or: [{
+            startTime: {
+                $gte: new Date()
+            },
+            $and: {
+                startTime: {
+                    $lt: new Date()
+                },
+                endTime: {
+                    $gte: new Date()
+                }
+            }
+        }],
+        isRegistrationRequired: false
+    }, { _id: 1});
+
+    openEventIds.forEach(async eventId => {
+        await Events.findByIdAndUpdate(eventId, { $push: { userId: this._id }});
+        await Events.find({ _id: eventId }).populate("registrations.userId");
+    });
+
+    next();
+});
 
 module.exports = mongoose.model("users", userSchema);
