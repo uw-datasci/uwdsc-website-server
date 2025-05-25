59cff5f40512151354baab244a2bc3900bba274f
JackChujun <1yanjac@gmail.com>
2025-05-25 11:30:07 -0700

    Modify check in so any user can check themselves in

--- controllers/registrantController.js
+++ controllers/registrantController.js
@@ -127,16 +127,22 @@ const attachRegistrantById = asyncHandler(async (req, res) => {
 //@access Private
 const checkInRegistrantById = asyncHandler(async (req, res) => {
   const {event_id, user_id} = req.params;
-  const userSecret = req.body.eventSecret;
+
+  if (req.user.id !== user_id) {
+    res.status(403);
+    throw new Error("You are not authorized to check in another user.");
+  }
+  
+  // const userSecret = req.body.eventSecret;
   const event = await Event.findOne({ _id: event_id });
-  const eventSecret = user_id + process.env.ACCESS_TOKEN_SECRET + event.secretName;
+  // const eventSecret = user_id + process.env.ACCESS_TOKEN_SECRET + event.secretName;
   const registrantIndex = event.registrants.findIndex(
     (r) => r.user.toString() === user_id
   );
   const registrant = event.registrants[registrantIndex];
 
   if (event && registrant) {
-    if (await bcrypt.compare(eventSecret, userSecret)){
+    // if (await bcrypt.compare(eventSecret, userSecret)){
       if (!registrant.checkedIn) {
         registrant.checkedIn = true;
         await Event.findOneAndUpdate(
@@ -151,10 +157,10 @@ const checkInRegistrantById = asyncHandler(async (req, res) => {
         res.status(500)
         throw new Error("Registrant is already checked in")
       }
-    } else {
-      res.status(500)
-      throw new Error("Hash does not match")
-    }
+    // } else {
+    //   res.status(500)
+    //   throw new Error("Hash does not match")
+    // }
   } else {
     res.status(404)
     throw new Error("Event id is not valid or user is not registered")
