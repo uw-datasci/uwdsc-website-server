9fe289e98b7883b2103a72792760a9500a3ce2ef
Aaryan Patel <86172867+patel-aaryan@users.noreply.github.com>
2025-07-07 23:46:58 -0400

    removed createapplication (unused, patchapplication is used throughout

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const {
-  createApplication,
   getApplicationByUserId,
   getCurrentTerm,
   patchApplication,
 } = require("../controllers/applicationController");
-const { requiresAll } = require("../middleware/errorHandler");
 const { validateUser } = require("../middleware/validateTokenHandler");
 
 const router = express.Router();
 
-router.post(
-  "/submit",
-  validateUser,
-  requiresAll(["termApplyingFor", "personalInfo", "academicInfo", "resumeUrl"]),
-  createApplication
-);
 router.post("/save", validateUser, patchApplication);
 router.post("/my_application", validateUser, getApplicationByUserId);
 router.get("/currentTerm", getCurrentTerm);
